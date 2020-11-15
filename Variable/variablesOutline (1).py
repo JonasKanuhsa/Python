1003064c420b68be2c8dbad36bdeb8bbb4602855
JonasKanuhsa <73847057+JonasKanuhsa@users.noreply.github.com>
2020-11-14 23:10:35 -0600

    Variable Outline Jonas Kanuhsa

---
+++ Variable/variablesOutline (1).py
@@ -0,0 +1,60 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+z = 0
+
+b = 1.23
+
+string = "cat"
+
+5 = true 
+
+mm = (1,2,3)
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+a = apple
+
+A = bannana
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+X = min
+
+X = Min
+
+#4)
+#Make a variable and set it to a variable you have already created:
+A = bannana
+
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+Bm = (fruit,meat,milk)
+
+bb = meat 
+#6)
+#Make a String set to your name. Make a variable that is set to the first item
+#in that list:
+title = "Jonas"
+
+cc = fruit
+
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+
+Print:title
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+var == m 
+
+
+
