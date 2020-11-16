60c28e852ce87eae1132f25d9939bddff4fdfa38
JonasKanuhsa <73847057+JonasKanuhsa@users.noreply.github.com>
2020-11-16 17:49:15 -0600

    Jonas Kanuhsa CommentsOutline

---
+++ Comments/commentsOutline.py
@@ -0,0 +1,27 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#This is Jonas's single line comment
+
+#2) Make a multi-line comment.
+'''
+This
+is 
+my 
+multi
+-
+line
+comment
+'''
+
+
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+int = 5 # variable int equals 5
+print(int)
