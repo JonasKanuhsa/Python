87a886dd41ff7e4899f65860db34d8a2ca4e53df
JonasKanuhsa <73847057+JonasKanuhsa@users.noreply.github.com>
2020-11-17 11:32:00 -0600

    Jonas Kanuhsa Comments Drill

---
+++ Comments Drill/commentDrills.py
@@ -0,0 +1,45 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#hello
+
+#2) Make a multi-line comment.
+
+'''
+hello
+my name
+is Jonas
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+p = 0 #that is a variable 
+
+#4) Make a single line comment.
+
+#this is my single line comment
+
+#5) Make a multi-line comment.
+
+'''
+the
+packers
+are 
+good
+'''
