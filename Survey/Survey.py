d53fb39f7277d0aa3a5cc008e0241f011e407668
JonasKanuhsa <73847057+JonasKanuhsa@users.noreply.github.com>
2020-12-05 14:35:36 -0600

    Jonas Kanuhsa Survey

---
+++ Survey/Survey.py
@@ -0,0 +1,37 @@
+'''
+Make a program that can take survey information.
+
+@author: Jonas Kanuhsa
+'''
+
+question = "What is your name?"
+
+name = input("What is your name")
+
+print(name)
+
+response = "What is your favorite color?"
+
+color = input("What is your favorite color?")
+
+print(color) 
+
+
+var = = "What city did you grow up in?"
+
+city = input("What city did you grow up in?")
+
+print(city)
+
+
+var1 = "What is your best friends name?"
+
+variable = input("What is your best friends name?")
+
+print(variable)
+
+var2 = "What color hair do you have?"
+
+hair = input("What color hair do you have?")
+
+print(hair)
