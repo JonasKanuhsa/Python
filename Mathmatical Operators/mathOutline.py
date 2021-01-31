e50c5c87270a72bd3cf0233995860299b1ff4fa1
JonasKanuhsa <73847057+JonasKanuhsa@users.noreply.github.com>
2021-01-31 12:58:33 -0600

    Create mathOutline.py

---
+++ Mathmatical Operators/mathOutline.py
@@ -0,0 +1,65 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+
+var = "one" + " new" + " variable"
+
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+
+y = 4 + 4
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+
+x = 8 - 4 
+
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+
+z = 4.5 - 2.5
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+
+v = 4 * 4
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+
+j = "jonas" * 3 
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+
+m = 9 / 3 
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+
+g = 4.3 / 5.2
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+
+r = 3 % 3
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+
+o = 8 // 7
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+
+w = 3 ** 5
+
+#12) Make any type of operation with a common SYNTAX error
+
+t = 9 5
+
