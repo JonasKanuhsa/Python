5e6899df2e79a51cf26273520edfb4d85d5fb352
JonasKanuhsa <73847057+JonasKanuhsa@users.noreply.github.com>
2021-03-14 17:02:58 -0500

    Create conditionaloutline.py

---
+++ Condition Statement/conditionaloutline.py
@@ -0,0 +1,45 @@
+'''
+Created on Mar 14, 2021
+
+@author: ITAUser
+'''
+#1) Make a int variable named a. Now make an if statement, and if a is more than
+#5, print out "a is more than 5."
+a = 5
+if(a > 5):
+    print("a is more than 5")
+
+
+#2) Make a boolean variable named b. Now make an if statement, and if b is True
+#print out "b is True."
+b = 0
+if(b==0):
+    print("b is True")
+
+#3) Make an int variable named c. Now make an if/elif statement, and if c is 
+#more than 0, print "c is positive". Else if c is less than 0, print "c is
+#negative".
+c = 1
+if(c > 0):
+    print("c is postitive")
+else:
+    print("c is negative")
+
+
+#4) Make an int variable named d. Now make an if/elif/else statement, and if 
+#d is more than 0, print "d is positive". Else if d is less than 0, print "c is
+#negative". Else, print "d is zero".
+d = -1
+if(d > 0):
+    print("d is positive")
+elif(d < 0):
+    print("d is negative")
+else:
+    print("d is zero")
+
+
+#5) Make an if statement with any comparison, with a common SYNTAX error.
+f = 1
+if(f > 0)
+print("f is positive")
+
