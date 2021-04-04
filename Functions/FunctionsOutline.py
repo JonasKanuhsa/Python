90604b4d1c63be2822f007298a1cf13a0eeb23f0
JonasKanuhsa <73847057+JonasKanuhsa@users.noreply.github.com>
2021-04-03 21:48:57 -0500

    Create FunctionsOutline.py

---
+++ Functions/FunctionsOutline.py
@@ -0,0 +1,33 @@
+'''
+Created on Apr 3, 2021
+
+@author: ITAUser
+'''
+'''
+This outline will help solidify concepts from the Functions lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a function that has two boolean parameters. If both booleans are
+#true, return true. Else, return false. Then, call the function.
+#Print what the function returns.
+x = [1 > 2]
+y = [2 == 2]
+def cars (x , y):
+    if True: 
+        print(cars)
+    return False
+
+
+#2) Make a function that takes one int parameter: gallons. Convert gallons
+#to cups (do this by multiplying gallons by 16). Then return cups. Then,
+#call the function.
+#Print what the function returns.
+
+
+
+#3) Make a function of any any kind with a common SYNTAX error. Then, call the
+#function.
+#Print what the function returns.
+
+
